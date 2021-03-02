cd678bbe34b132c50492fc62519cbf0ca6feea9a
jnasser <jnasser@broadinstitute.org>
2021-03-02 17:01:37 +0000

    added Roadmap and PCHiC comparisons

---
+++ comparePredictorsToCRISPRData/comparisonRuns/RoadmapComparison-RoadmapMatched/runComparison.sh
@@ -0,0 +1,9 @@
+Rscript ../../code/comparePredictionsToExperiment.R \
+                --predictions config/pred.table.listing.txt \
+                --experimentalData experimentalData/experimentalData.RoadmapComparison-RoadmapMatched.txt \
+                --experimentalPositiveColumn 'Regulated' \
+                --cellNameMapping config/cellNameMapping.txt \
+                --plotConfig config/plot.config.txt \
+                --predConfig ../../config/pred.config.txt \
+                --code ../../code/comparison.R \
+                --outDir out/
