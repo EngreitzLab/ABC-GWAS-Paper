07d819286bfee2a46d4b6f231564d49bc71a0bf9
jnasser <jnasser@broadinstitute.org>
2021-03-05 18:37:13 +0000

    remove deleted files

--- comparePredictorsToCRISPRData/comparisonRuns/AllCellTypes-ABC_comparison/runComparison.AllCellTypes-ABC_comparison.sh
+++
@@ -1,9 +0,0 @@
-Rscript ../../code/comparePredictionsToExperiment.R \
-                --predictions config/pred.table.listing.txt \
-                --experimentalData experimentalData/experimentalData.AllCellTypes.txt \
-                --experimentalPositiveColumn 'Regulated' \
-                --cellNameMapping config/cellNameMapping.txt \
-                --plotConfig config/plot.config.txt \
-                --predConfig ../../config/pred.config.txt \
-                --code ../../code/comparison.R \
-                --outDir out/
